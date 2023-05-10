e3665237bc7b413470b24e33b9bcafb9087d2e95
Екатерина Седова <katena.sedova@gmail.com>
2023-05-10 23:15:16 +0300

    'ready'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import AppRouter from './components/AppRouter'
 import ShopNavBar from "./components/ShopNavBar";
-function App() {
-  return (
-    <BrowserRouter>
-        <ShopNavBar />
-        <AppRouter />
-    </BrowserRouter>
-  );
-}
+import {observer} from "mobx-react-lite";
+import {Context} from "./index";
+import {check} from "./http/userAPI";
+import {Spinner} from "react-bootstrap";
+import {fetchBiscuits, fetchCakes, fetchFillings} from "./http/CakeAPI";
+
+const App = observer( () => {
+    const {user} = useContext(Context)
+    const {cakes} = useContext(Context)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        check().then(data => {
+            user.setUser(true)
+            user.setIsAuth(true)
+        }).finally(() => setLoading(false))
+    }, [])
+
+    if(loading) {
+        return <Spinner animation={"grow"}/>
+    }
+
+    return (
+        <BrowserRouter>
+            <ShopNavBar />
+            <AppRouter />
+        </BrowserRouter>
+    );
+});
 
 export default App;
